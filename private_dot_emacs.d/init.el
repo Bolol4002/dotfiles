b7c4eb33edaaa02041ef501ba762f349a8fe7861
bolol4002 <sayoojsumesh1112@gmail.com>
2026-03-02 21:50:01 +0530

    lot

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,22 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-dired all-the-icons-ivy-rich company-box counsel
+			 dashboard diminish dired-open
+			 eshell-syntax-highlighting eshell-toggle
+			 evil-collection flycheck general neotree
+			 org-bullets peep-dired projectile
+			 rainbow-mode sudo-edit toc-org vterm-toggle)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
