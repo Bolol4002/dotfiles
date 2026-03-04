c75725038f9d7519c9d3c5adc842f77bbd8d72cb
bolol4002 <sayoojsumesh1112@gmail.com>
2026-03-04 11:52:24 +0530

    wm

--- dot_config/mango/executable_autostart.sh
+++ dot_config/mango/executable_autostart.sh
@@ -1,3 +1,3 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 # Use the wallpaper shipped inside this repo
-swaybg -i ~/.config/mango/wallpaper/mywall.png -m fill >/dev/null 2>&1 &
+swaybg -i ~/.config/mango/wallpaper/wall2.jpg -m fill >/dev/null 2>&1 &
